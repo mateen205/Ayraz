d6f965b6aff1717fec1ca2a9ce557d98f4130b0a
mateen205 <mateenmalik1526@gmail.com>
2026-08-24 22:08:37 +0500

    Connect database using environment variables

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,10 +1,17 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ayraz_store",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl:
+    process.env.DB_SSL === "true"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : undefined,
   waitForConnections: true,
   connectionLimit: 10,
 });
